7ee48664cca55d2eac0e0eb760db1836fd67de1e
zyfoolboy <zyfoolboy@gmail.com>
2016-06-08 22:48:18 +0800

    push project

--- ButtonTitle/ButtonTitle/ViewController.m
+++ ButtonTitle/ButtonTitle/ViewController.m
@@ -16,6 +16,20 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    btn.frame = CGRectMake(100, 100, 60, 30);
+    
+    
+    [btn setImage:[UIImage imageNamed:@"item_grid_filter_price_arrow"] forState:UIControlStateNormal];
+    
+    [btn setTitle:@"选择" forState:UIControlStateNormal];
+    [btn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+   // btn.titleEdgeInsets = UIEdgeInsetsMake(10, 30, 0, 30);
+    btn.imageEdgeInsets = UIEdgeInsetsMake(5, 35, 5, 0);
+    
+    [self.view addSubview:btn];
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
